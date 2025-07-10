67f9340c12504535ab06c444328fd95b2187b4c2
@123umair <khanuumair91@gmail.com>
2025-07-10 16:01:27 +0500

    Updated

---
+++ Encapsulation.py
@@ -0,0 +1,17 @@
+# When we initialize any variable in python with double underscore __  then this vairable will be private like __hello = 'somethng' now this vairable is private
+# And this vairalbe will be access during the class and not accessible for any instace or object of the class.
+class Encapsulation:
+    def __init__(self,name,age):
+        self.__name = name
+        self.age = age   #now this vairable is not  private now we will access its outside the class easily
+        self.__age = age
+    def Method(self):
+        return f"{self.__name}"
+
+object = Encapsulation('umair',38)
+#  it will give the value 38
+print(object.age)  
+# Correct way 1: Using the class method
+print("Accessing through method:", object.Method())
+# Correct way 2: Using name mangling
+print("Accessing through name mangling:", object._Encapsulation__name)
---
+++ listFunction.py
@@ -0,0 +1,8 @@
+from turtle import end_fill
+
+
+def check_length(cities):
+    for i in cities:
+        print( i , end = " ")
+cities = ['umair','agb','junaid']
+check_length(cities)
